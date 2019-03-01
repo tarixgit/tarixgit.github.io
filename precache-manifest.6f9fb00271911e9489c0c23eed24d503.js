8d005f1a6bc8a09632557871125e5805475dd2ad
tarix <tarix@zedat.fu-berlin.de>
2019-03-01 02:06:09 +0100

    - redeploy

---
+++ precache-manifest.6f9fb00271911e9489c0c23eed24d503.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7507c34f2fdcfb9deed0",
+    "url": "/static/js/main.7507c34f.chunk.js"
+  },
+  {
+    "revision": "b26f9c251547009a04f7",
+    "url": "/static/js/1.b26f9c25.chunk.js"
+  },
+  {
+    "revision": "7507c34f2fdcfb9deed0",
+    "url": "/static/css/main.3a495ce4.chunk.css"
+  },
+  {
+    "revision": "bd41cf1069679bff9422da709eef8db4",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df3f86216db8abade2a6d95df4a542cf.js"
+  "/precache-manifest.6f9fb00271911e9489c0c23eed24d503.js"
 );
 
 workbox.clientsClaim();
